4408096a5024212314bbb7822739cd90769f0308
paulocdeveloper <gow.luukz122@gmail.com>
2023-08-20 15:05:10 -0300

    firts commit

---
+++ script.js
@@ -0,0 +1,87 @@
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal({duration:2000})
+
+sr.reveal('.conhecimentos' , {
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+});
+
+sr.reveal('.content-about',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ });
+sr.reveal('.sobre-title',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+sr.reveal('.caixa-sobre',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+sr.reveal('.projetos',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+sr.reveal('.content-projetos',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.content-about2',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('#paulo-img',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.text-position',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.projetos',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.projeto1',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.projeto2',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.projeto2',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.contato',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.content-contato',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+
+ sr.reveal('.email',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ })
+ 
+ sr.reveal('.whatsapp-contato',{
+    rotate:{ x:100, y:10, z:0},
+    duration:2000
+ }) 
